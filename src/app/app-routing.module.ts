7f43678ca1642632038b4b948cbfccbb9b9c5cd1
Vitor Gesteira <dev.vitorgesteira@gmail.com>
2023-01-11 10:56:06 -0300

    roteamento de paginas

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router'
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
+import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
+import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
+import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+
+// Arquivo responsavel por fazer o roteamento
+
+const routes: Routes = [
+  { path: "primeira-pagina", component: PrimeiraPaginaComponent },
+  { path: "segunda-pagina", component: SegundaPaginaComponent },
+  { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
+  { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
+  { path: "", redirectTo: "primeira-pagina", pathMatch: "full" },
+  { path: "**", component: PaginaNaoEncontradaComponent }
+
+]
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/segunda-pagina/segunda-pagina.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-segunda-pagina',
+  templateUrl: './segunda-pagina.component.html',
+  styleUrls: ['./segunda-pagina.component.css']
+})
+export class SegundaPaginaComponent {
+  constructor(
+    private router: Router
+  ){}
+
+  moverParaPrimeiraPagina(){
+    this.router.navigate(["primeira-pagina"])
+  }
+}
